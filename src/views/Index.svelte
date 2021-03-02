dd907f1fe3c72753d4495d1a1342bac7cd15b44e
John <john@real-currents.com>
2021-03-02 11:08:30 -0500

    update styles

--- src/views/Index.svelte
+++ src/views/Index.svelte
@@ -26,14 +26,13 @@
 <style>
   main {
     text-align: center;
-    padding: 1em;
-    padding-top: 64px;
+    padding: 64px 1em 1em;
     max-width: 240px;
     margin: 0 auto;
   }
 
   h1 {
-    color: #ff3e00;
+    color: #f56565;
     text-transform: uppercase;
     font-size: 4em;
     font-weight: 100;
